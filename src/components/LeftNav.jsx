98b5c4e41c54fef2edac278d8fc832b31b6bbc9b
Manishkmr406 <manishkmr406@gmail.com>
2023-12-28 16:26:28 +0530

    updated

--- src/components/LeftNav.jsx
+++ src/components/LeftNav.jsx
@@ -51,7 +51,7 @@ const LeftNav = () => {
         })}
         <hr className="my-5 border-white/[0.2]" />
         <div className="text-white/[0.5] text-[12px]">
-          Clone by : Manish kumar
+          Clone by: Manish kumar
         </div>
       </div>
     </div>
